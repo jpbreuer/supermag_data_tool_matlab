efd5b1a27247b8c9307301fa49f816e429586abe
jpbreuer <jeanpaul.breuer@gmail.com>
2015-11-06 22:43:52 +0100

    changed name of function for clarity

--- issiworkshop.m
+++ issiworkshop.m
@@ -3,10 +3,10 @@ clear all
 close all
 
 % Specify dataset
-filepath = './supermag_data/csv/2005-Jan-21_unsorted.csv';
+filepath = './supermag_data/csv/2010-May-03.csv';
 
 % Load data
-[Date_UTC, jd2000, IAGA, MLT, MLAT, IGRF_DECL, SZA, N, E, Z] = data2matrix(filepath);
+[Date_UTC, jd2000, IAGA, MLT, MLAT, IGRF_DECL, SZA, N, E, Z] = supermagdata2matrix(filepath);
 
 % IAGA variable contains relative indexed information for all available
 % stations (alphabetized). Each station index corresponds to a column in any of the data
