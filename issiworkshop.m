c70f7ec07ede7109ab1cfd29a727bb03fa6ab904
jpbreuer <jeanpaul.breuer@gmail.com>
2016-02-25 13:31:29 +0100

    really fixed the julian day

--- issiworkshop.m
+++ issiworkshop.m
@@ -6,7 +6,7 @@ close all
 filepath = './supermag_data/csv/2010-May-03.csv';
 
 % Load data
-[jd2000, IAGA, MLT, MLAT, IGRF_DECL, SZA, N, E, Z] = supermagdataall2matrix(filepath);
+[jd2000exact, IAGA, MLT, MLAT, IGRF_DECL, SZA, N, E, Z] = supermagdataall2matrix(filepath);
 
 % IAGA variable contains relative indexed information for all available
 % stations (alphabetized). Each station index corresponds to a column in any of the data
